d26011c692e2b02403aff66b24f523fac48d6ea4
PhamTrungHieu20204968 <phamhieu15082002@gmail.com>
2024-05-12 18:10:12 +0700

    deploy frontend

--- client/postcss/config.js
+++ client/postcss/config.js
@@ -3,6 +3,7 @@ module.exports = {
     "postcss-import": {},
     "tailwindcss/nesting": "postcss-nesting",
     tailwindcss: {},
+    autoprefixer: {},
     "postcss-preset-env": {
       features: { "nesting-rules": false },
     },
