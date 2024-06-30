448fb915d37ded59cefc07366fdb08619ae1cbd7
PhamTrungHieu20204968 <phamhieu15082002@gmail.com>
2024-06-30 11:01:42 +0700

    final

--- client/src/features/blog/components/BlogMenu.js
+++ client/src/features/blog/components/BlogMenu.js
@@ -1,9 +1,9 @@
-import { Popconfirm, message } from "antd";
-import React from "react";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Popconfirm, message } from 'antd';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
-import { useDeleteBlogMutation } from "app/api/blogService";
+import { useDeleteBlogMutation } from 'app/api/blogService';
 function BlogMenu({ blog }) {
   const [deleteBlog] = useDeleteBlogMutation();
   const { accessToken, userId, role, language } = useSelector(
@@ -19,11 +19,13 @@ function BlogMenu({ blog }) {
     })
       .then((res) => {
         if (res.data?.error) {
-          if (language === "vi") {
+          if (language === 'vi') {
             message.error(res.data.error);
-          } else message.error("失敗しました");
+          } else message.error('失敗しました');
         } else {
-          message.success(language === "vi" ? "Xóa thành công" : "削除に成功しました");
+          message.success(
+            language === 'vi' ? 'Xóa thành công' : '削除に成功しました'
+          );
         }
       })
       .catch((err) => {
@@ -31,33 +33,25 @@ function BlogMenu({ blog }) {
       });
   };
   return (
-    <div className='z-40'>
-      <div className='text-base mb-2 last:mb-0 cursor-pointer hover:bg-gray-200 rounded-md p-2'>
-        {language === "vi" ? "Báo cáo bài viết" : "報告する"}
-      </div>
-      {(userId === blog.userId || role > 1) && (
-        <div className='text-base mb-2 last:mb-0 cursor-pointer hover:bg-gray-200 rounded-md p-2'>
-          {language === "vi" ? "Gỡ bài viết" : "抜く"}
-        </div>
-      )}
+    <div className="z-40">
       {(userId === blog.userId || role > 1) && (
         <div
-          className='text-base mb-2 last:mb-0 cursor-pointer hover:bg-gray-200 rounded-md p-2'
+          className="text-base mb-2 last:mb-0 cursor-pointer hover:bg-gray-200 rounded-md p-2"
           onClick={() => navigate(`/blogs/${blog.id}`)}
         >
-          {language === "vi" ? "Sửa bài viết" : "編集する"}
+          {language === 'vi' ? 'Sửa bài viết' : '編集する'}
         </div>
       )}
       {(userId === blog.userId || role > 1) && (
         <Popconfirm
-          title='Xóa bài viết'
-          description='Bạn muốn xóa bài viết này?'
-          okText={language === "vi" ? "Có" : "オーケー"}
-          cancelText={language === "vi" ? "Không" : "いいえ"}
+          title="Xóa bài viết"
+          description="Bạn muốn xóa bài viết này?"
+          okText={language === 'vi' ? 'Có' : 'オーケー'}
+          cancelText={language === 'vi' ? 'Không' : 'いいえ'}
           onConfirm={handleDeleteBlog}
         >
-          <div className='text-base mb-2 text-red-500 last:mb-0 cursor-pointer hover:bg-gray-200 rounded-md p-2'>
-            {language === "vi" ? "Xóa bài viết" : "削除する"}
+          <div className="text-base mb-2 text-red-500 last:mb-0 cursor-pointer hover:bg-gray-200 rounded-md p-2">
+            {language === 'vi' ? 'Xóa bài viết' : '削除する'}
           </div>
         </Popconfirm>
       )}
